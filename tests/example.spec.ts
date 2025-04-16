501ed5e8d2f8c7a6277e2e3437ee6c5bc9b70c39
Victor Ferreira <git@victorferreira.me>
2025-04-16 12:39:54 -0300

    initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  // await page.goto('https://playwright.dev/');
+  await page.goto("http://localhost:8080");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("Welcome to nginx!");
+});
